437d47b955a17efb007f7a9d9b44122f3c2cd58f
Santisteban <san21153@uvg.edu.gt>
2024-03-04 16:38:27 -0600

    mejoras de organizacion

--- Proyecto.py
+++ Proyecto.py
@@ -1,5 +1,6 @@
 from Preprocessing import *
 from Search import *
+import time 
 
 # Llamar a la función de búsqueda deseada
 print("Laberinto:")
@@ -9,63 +10,107 @@ start, end = find_start_end_coordinates(maze)
 print("Coordenadas de inicio:", start)
 print("Coordenadas de fin:", end)
 
+salida = True
 
-# Ejemplo de uso de BFS
-print("\nEjemplo de búsqueda usando BFS:")
-path_bfs = bfs(start, end, maze )
-#contar en cuantos pasos se resuelve el laberinto
-pasos = len(path_bfs)
-if path_bfs:
-    print("Camino encontrado:", path_bfs)
-    print("Pasos para resolver el laberinto: ", pasos)
-else:
-    print("No se encontró ningún camino usando BFS.")
+while salida:
+    
+    print('----------------MENU DE OPCIONES----------------')
+    print('Seleccione el algoritmo de búsqueda que desea usar: ')
+    print('1. BFS')
+    print('2. A*')
+    print('3. DFS')
+    print('4. Greedy First Search')
+    print('5. Depth Limited Search')
+    print('6. Salir')
 
+    opcion = int(input('Ingrese la opción que desea: '))
 
-# Ejemplo de uso de A*
-print("\nEjemplo de búsqueda usando A*:")
-heuristic_functions = create_heuristic_functions(maze, end, euclidean_distance)
-path_a_star = a_star(start, end, maze, heuristic_functions)
-pasos = len(path_a_star)
-if path_a_star:
-    print("Camino encontrado:", path_a_star)
-    print("Pasos para resolver el laberinto: ", pasos)
-else:
-    print("No se encontró ningún camino usando A*.")
+    if opcion == 1:
 
+        # Ejemplo de uso de BFS
+        print("\nEjemplo de búsqueda usando BFS:")
 
-# Ejemplo de uso de DFS
-print("\nEjemplo de búsqueda usando DFS:")
+        start_time = time.time()
 
-path_dfs = dfs(start, end, maze)
-pasos = len(path_dfs)
-if path_dfs:
-    print("Camino encontrado:", path_dfs)
-    print("Pasos para resolver el laberinto: ", pasos)
-else:
-    print("No se encontró ningún camino usando DFS.")
+        path_bfs = bfs(start, end, maze )
+        #contar en cuantos pasos se resuelve el laberinto
+        
+        excecution_time = round(time.time() - start_time, 6)
+        
+        if path_bfs:
+            pasos = len(path_bfs)
+            print("Camino encontrado:", path_bfs)
+            print("Pasos para resolver el laberinto: ", pasos)
+            print("--- %s seconds ---" % excecution_time)
+        else:
+            print("No se encontró ningún camino usando BFS.")
 
-# Ejemplo de uso de Greedy First Search
-print("\nEjemplo de búsqueda usando Greedy First Search:")
+    elif opcion == 2:
 
-path_greedy_first_search = greedy_first_search(start, end, maze, heuristic_functions)
-pasos = len(path_greedy_first_search)
-if path_greedy_first_search:
-    print("Camino encontrado:", path_greedy_first_search)
-    print("Pasos para resolver el laberinto: ", pasos)
-else:
-    print("No se encontró ningún camino usando Greedy First Search.")
+        # Ejemplo de uso de A*
+        print("\nEjemplo de búsqueda usando A*:")
+        heuristic_functions = create_heuristic_functions(maze, end, euclidean_distance)
 
-# Ejemplo de uso de Depth Limited Search
-print("\nEjemplo de búsqueda usando Depth Limited Search:")
-path_depth_limited_search = depth_limited_search(start, end, 10, maze)
+        start_time = time.time()
 
-if path_depth_limited_search:
-    print("Camino encontrado:", path_depth_limited_search)
-    pasos = len(path_depth_limited_search)
-    print("Pasos para resolver el laberinto: ", pasos)
-else:
-    print("No se encontró ningún camino usando Depth Limited Search.")
+        path_a_star = a_star(start, end, maze, heuristic_functions)
+        excecution_time = round(time.time() - start_time, 6)
+        if path_a_star:
+            pasos = len(path_a_star)
+            print("Camino encontrado:", path_a_star)
+            print("Pasos para resolver el laberinto: ", pasos)
+            print("--- %s seconds ---" % excecution_time)
+        else:
+            print("No se encontró ningún camino usando A*.")
+
+    elif opcion == 3:
+        # Ejemplo de uso de DFS
+        print("\nEjemplo de búsqueda usando DFS:")
+        start_time = time.time()
+        path_dfs = dfs(start, end, maze)
+        
+        excecution_time = round(time.time() - start_time, 6)
+        if path_dfs:
+            pasos = len(path_dfs)
+            print("Camino encontrado:", path_dfs)
+            print("Pasos para resolver el laberinto: ", pasos)
+            print("--- %s seconds ---" % excecution_time)
+        else:
+            print("No se encontró ningún camino usando DFS.")
+
+    elif opcion == 4:
+        # Ejemplo de uso de Greedy First Search
+        print("\nEjemplo de búsqueda usando Greedy First Search:")
+        start_time = time.time()
+        path_greedy_first_search = greedy_first_search(start, end, maze, heuristic_functions)
+        
+        excecution_time = round(time.time() - start_time, 6)
+        if path_greedy_first_search:
+            pasos = len(path_greedy_first_search)
+            print("Camino encontrado:", path_greedy_first_search)
+            print("Pasos para resolver el laberinto: ", pasos)
+            print("--- %s seconds ---" % excecution_time)
+        else:
+            print("No se encontró ningún camino usando Greedy First Search.")
+
+    elif opcion == 5:
+        # Ejemplo de uso de Depth Limited Search
+        print("\nEjemplo de búsqueda usando Depth Limited Search:")
+        start_time = time.time()
+        path_depth_limited_search = depth_limited_search(start, end, 10, maze)
+        excecution_time = round(time.time() - start_time, 6)
+        if path_depth_limited_search:
+            print("Camino encontrado:", path_depth_limited_search)
+            pasos = len(path_depth_limited_search)
+            print("Pasos para resolver el laberinto: ", pasos)
+            print("--- %s seconds ---" % excecution_time)
+            
+        else:
+            print("No se encontró ningún camino usando Depth Limited Search.")
+
+    else:
+        print('Saliendo...')
+        salida = False
 
 
 
